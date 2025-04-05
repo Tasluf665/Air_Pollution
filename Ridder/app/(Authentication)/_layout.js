dca46754945f5dcb46a7216755bb6c1afc596b15
Tasluf665 <tasluf665@gmail.com>
2025-04-05 04:03:00 +0300

    Fixing Bug

--- Ridder/app/(Authentication)/_layout.js
+++ Ridder/app/(Authentication)/_layout.js
@@ -7,9 +7,6 @@ export default () => {
             screenOptions={{ headerShown: false }}
         >
             <Stack.Screen name="SplashScreen" />
-            <Stack.Screen name="WelcomeScreen" />
-            <Stack.Screen name="LoginScreen" />
-            <Stack.Screen name="SignupScreen" />
         </Stack>
     );
 };
